e4b57a0d38facba428f87bd664a6f35ac4ad6b67
developerkamol <developerkamol@gmail.com>
2025-03-29 09:11:42 +0500

    Initial Commit

---
+++ MathWhiz/MathWhizApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathWhizApp.swift
+//  MathWhiz
+//
+//  Created by Kamol Madaminov on 29/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MathWhizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
